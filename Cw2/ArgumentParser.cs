26a26e624777f8683f7c6821f02b6e8083e8ef90
Paweł <73314096+s18277@users.noreply.github.com>
2020-11-01 18:16:21 +0100

    Add actual student's data conversion and argument handling

---
+++ Cw2/ArgumentParser.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using Cw2.Serializers;
+using static Cw2.Logger;
+
+namespace Cw2
+{
+    /// <summary>
+    ///     <para>
+    ///         Klasa pomocnicza veryfikująca poprawność przekazanych do programu argumentów.
+    ///         Jest stworzona w sposób generyczny, więc nie trzeba jej modyfikować w przypadku zmiany danych do parsowania.
+    ///         Będzie wymagała jedynie modyfikacji w momencie, gdy chcemy dodać nowy sposób zapisu danych
+    ///         (metoda <see cref="DetermineCorrectSerializer" />).
+    ///     </para>
+    ///     <para>
+    ///         Przechowuje obiekty <see cref="InputReader" /> i <see cref="OutputWriter" /> odpowiedzialne za odczyt i zapis
+    ///         danych do pliku oraz obiekt <see cref="ObjectSerializer" /> odpowiedzialny za serializację odczytanych danych.
+    ///     </para>
+    /// </summary>
+    /// <typeparam name="T">Typ danych które zostaną zapisane do pliku.</typeparam>
+    public class ArgumentParser<T>
+    {
+        private const int InputPathIndex = 0;
+        private const int OutputPathIndex = 1;
+        private const int SerializationFormatIndex = 2;
+        private readonly XmlSerializerNamespaces _xmlSerializerNamespaces;
+        private readonly XmlWriterSettings _xmlWriterSettings;
+
+        public ArgumentParser(IReadOnlyList<string> arguments, XmlWriterSettings xmlWriterSettings = null,
+            XmlSerializerNamespaces xmlSerializerNamespaces = null)
+        {
+            _xmlSerializerNamespaces = xmlSerializerNamespaces;
+            _xmlWriterSettings = xmlWriterSettings;
+            VerifyNumberOfArguments(arguments);
+            InputReader = new StreamReader(TryToOpenPath(arguments[InputPathIndex], File.OpenRead));
+            ObjectSerializer = DetermineCorrectSerializer(arguments[SerializationFormatIndex]);
+            OutputWriter = OpenAndClearOutput(arguments[OutputPathIndex]);
+        }
+
+        public TextReader InputReader { get; }
+        public TextWriter OutputWriter { get; }
+        public IObjectSerializer<T> ObjectSerializer { get; }
+
+        private static void VerifyNumberOfArguments(IReadOnlyCollection<string> arguments)
+        {
+            if (arguments.Count == 3 && !arguments.Any(string.IsNullOrWhiteSpace)) return;
+            var numberOfTrimmedArguments = arguments.Count(argument => !string.IsNullOrWhiteSpace(argument));
+            throw LogException(
+                new ArgumentException(
+                    $"Niepoprawna liczba argumentów! Wymagana 3, Podano {numberOfTrimmedArguments}!"));
+        }
+
+        /// <summary>
+        ///     Metoda ustalająca sposób zapisu danych do pliku poprzez wybranie odpowiedniej implementacji interfesu
+        ///     <see cref="IObjectSerializer{T}" />. W przypadku dodania nowego sposobu konstrukcja <c>switch</c>
+        ///     powinna zostać rozszerzona o nowe opcje.
+        /// </summary>
+        /// <param name="serializerName">argument przekazany do programu określający rodzaj serializacji</param>
+        /// <returns>obiekt odpowiedniej klasy serializującej dane</returns>
+        private IObjectSerializer<T> DetermineCorrectSerializer(string serializerName)
+        {
+            return serializerName.ToLower() switch
+            {
+                "xml" => new XmlSerializer<T>(_xmlWriterSettings, _xmlSerializerNamespaces),
+                "json" => new JsonSerializer<T>(),
+                _ => throw LogException(
+                    new ArgumentException($"Podano nieoczekiwany format wyjściowy: {serializerName}!"))
+            };
+        }
+
+        private static TextWriter OpenAndClearOutput(string outputPath)
+        {
+            var inputStream = TryToOpenPath(outputPath, File.OpenWrite);
+            inputStream.SetLength(0);
+            inputStream.Flush();
+            return new StreamWriter(inputStream);
+        }
+
+        private static FileStream TryToOpenPath(string path, Func<string, FileStream> openFunction)
+        {
+            try
+            {
+                return openFunction(path);
+            }
+            catch (Exception ex) when (ex is PathTooLongException || ex is ArgumentException)
+            {
+                throw LogException(new ArgumentException($"Niepoprawna ścieżka: {path}!"));
+            }
+            catch (Exception ex) when (ex is FileNotFoundException || ex is DirectoryNotFoundException)
+            {
+                throw LogException(new FileNotFoundException($"Plik o nazwie {path} nie istnieje!"));
+            }
+            catch (Exception ex) when (ex is UnauthorizedAccessException || ex is NotSupportedException ||
+                                       ex is IOException)
+            {
+                throw LogException(new IOException($"Brak dostępu do pliku {path}!"));
+            }
+        }
+    }
+}
---
+++ Cw2/UniversityDataConverter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Cw2.Models;
+using Cw2.Parsers;
+using Cw2.Parsers.Fields;
+using static System.Text.Encoding;
+
+namespace Cw2
+{
+    /// <summary>
+    ///     Główna klasa z funkcją <c>Main</c>.
+    ///     Tworzy obiekty parsera danych <see cref="IObjectParser{T}" />, konwertera danych <see cref="DataConverter{T,TU}" />
+    ///     do innego formatu i pomocnicze klasy <see cref="Logger" /> odpowiedzialne za logowanie błędów
+    ///     i <see cref="ArgumentParser{T}" /> zajmujące się weryfikacją i parsowaniem przekazanych argumentów.
+    /// </summary>
+    internal static class UniversityDataConverter
+    {
+        private const string Author = "Paweł Rutkowski";
+        private const string ErrorLogPath = "log.txt";
+        private const string Delimiter = ",";
+
+        private static void Main(string[] arguments)
+        {
+            ConfigureErrorLogging();
+            PrepareUniversityDataConverter(ParseArguments(arguments)).Convert();
+        }
+
+        private static void ConfigureErrorLogging()
+        {
+            var logFile = File.OpenWrite(ErrorLogPath);
+            logFile.SetLength(0);
+            logFile.Flush();
+            var errorWriter = new StreamWriter(logFile);
+            Logger.SetErrorWriter(errorWriter);
+        }
+
+        private static DataConverter<ICollection<Student>, UniversityData> PrepareUniversityDataConverter(
+            ArgumentParser<UniversityData> arguments)
+        {
+            var studentParser = new LineDelimitedParser<Student>(Delimiter, new FieldsToStudentParser());
+            return new DataConverter<ICollection<Student>, UniversityData>(studentParser, arguments.ObjectSerializer,
+                arguments.InputReader, arguments.OutputWriter, students => new UniversityData(students, Author));
+        }
+
+        private static ArgumentParser<UniversityData> ParseArguments(IReadOnlyList<string> arguments)
+        {
+            var xmlSerializerNamespaces = new XmlSerializerNamespaces();
+            xmlSerializerNamespaces.Add("", "");
+            var xmlSettings = new XmlWriterSettings {Indent = true, OmitXmlDeclaration = true, Encoding = Default};
+            return new ArgumentParser<UniversityData>(arguments, xmlSettings, xmlSerializerNamespaces);
+        }
+    }
+}
